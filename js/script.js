d8f778a1367c76ebdd1a47d18a4038672c871f2e
SparqueeZ <baptiste.dijouxn8@gmail.com>
2023-03-21 15:24:36 +0100

    Ajout de la fonctionnalité de carte déroulante sur la page d'accueil

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-//alert("This alert box was called with the onload event");
-    for (nbr_cartes=0; nbr_cartes<=5; nbr_cartes++){
-      $( ".content"+nbr_cartes ).hover(
-        function() {
-          for (carte=0; carte<=nbr_cartes; carte++){
-            $( ".content"+carte ).addClass( "width4" );
-          }
-        }, function() {
-          for (carte=0; carte<=nbr_cartes; carte++){
-            $( ".content"+carte ).removeClass( "width4" );
-          }
+const Slider = () => {
+  for (nbr_cartes=0; nbr_cartes<=5; nbr_cartes++){
+    $( ".content"+nbr_cartes ).hover(
+      function() {
+        for (carte=0; carte<=nbr_cartes; carte++){
+          $( ".content"+carte ).addClass( "width4" );
         }
-      );
-    }
+      }, function() {
+        for (carte=0; carte<=nbr_cartes; carte++){
+          $( ".content"+carte ).removeClass( "width4" );
+        }
+      }
+    );
+  }
+}    
+
+const DescDevelop = () => {
+  document.querySelectorAll(".cardV2RatingTabBtn").forEach((cardBtn) => {
+    cardBtn.addEventListener("click", (event) => {
+      cardDesc = event.target.closest(".cardv2desc");
+      cardHasOpen = cardDesc.classList.contains("open");
+      if ( cardHasOpen == false ) {
+        $(cardDesc).addClass('open');
+      } else {
+        $(cardDesc).removeClass("open");
+      }
+    });
+  });
+}
+
+Slider();
+DescDevelop();
