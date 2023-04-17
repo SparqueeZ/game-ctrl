79e5e4608a7db9ef35ff8fee2afe52ef0bf87b62
Baptiste <baptiste.dijouxn8@gmail.com>
2023-04-17 18:02:38 +0200

    Ajout : 1. Code javascript afin de générér les jeux sur la page d'accueil, page de jeu de test nommé 'game-pages'. 2. Ajout de 3 images de miniatures pour la génération des cartes de jeux sur la page d'accueil. 3. Ajout du fav-icon et du nom de la page d'accueil. Modification : 1. Changement de la page index.html afin qu'elle n'affiche plus les cartes de jeux par défaut. 2. Ré-organisation des fichiers javascripts.

---
+++ js/showGames.js
@@ -0,0 +1,169 @@
+const showListProduct = () => {
+
+    const dataJeux = get('http://localhost:3000/api/v1/jeux')
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        });
+
+    // Utilisation de l'asynchrone avec async await afin d'attendre la réponse de la promise avant d'éxécuter le code
+    const printGames = async () => {
+        const liste = await dataJeux;
+        console.log(liste.data[1].nom); //Accéder au nom du 2e element du tableau
+
+        const putGames = document.getElementById('container');
+        let game = '';
+        liste.data.forEach(jeu => {
+            game += `
+          <div class="cardv2">
+            <div class="card-left">
+              <img class="cardv2img" src="${jeu.img_miniature}" alt="" />
+              <span class="cardrating20">
+                <span class=" cardrating20in">
+                  <p class="rating">${jeu.note_globale}</p>
+                  <p class="rating">20</p>
+                </span>
+              </span>
+            </div>
+            <div class="card-right">
+              <span class="cardv2name"><h3>${jeu.nom}</h3><p>${jeu.nom_precision}</p></span>
+              <p class="cardv2phrase">${jeu.description_rapide}</p>
+              <span class="cardV2RatingContainer">
+                <span class="cardV2RatingSection">
+                  <span class="cardV2RatingIcon">
+                    <img src="img/icons/noun-gamepad-1608468.png" alt="">
+                  </span>
+                  <span class="cardV2RatingProgressBar">
+                    <span class="cardRatingIconBg"></span>
+                    <span class="cardRatingBarHolder">
+                      <span class="cardV2RatingBar">
+                        <p class="cardV2RatingPourcentage">${jeu.note_gameplay}%</p>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+                <span class="cardV2RatingSection">
+                  <span class="cardV2RatingIcon">
+                    <img src="img/icons/noun-wrench-1560053.png" alt=""></span>
+                  <span class="cardV2RatingProgressBar">
+                    <span class="cardRatingIconBg"></span>
+                    <span class="cardRatingBarHolder">
+                      <span class="cardV2RatingBar">
+                        <p class="cardV2RatingPourcentage">${jeu.note_technique}%</p>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+                <span class="cardV2RatingSection">
+                  <span class="cardV2RatingIcon">
+                    <img src="img/icons/noun-story-3266367.png" alt=""></span>
+                  <span class="cardV2RatingProgressBar">
+                    <span class="cardRatingIconBg"></span>
+                    <span class="cardRatingBarHolder">
+                      <span class="cardV2RatingBar">
+                        <p class="cardV2RatingPourcentage">${jeu.note_histoire}%</p>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+                <span class="cardBtnSection">
+                  <span class="cardBtnHolder">
+                    <span class="cardBtn">
+                      <span class="cardBtnIcon"></span>
+                    </span>
+                  </span>
+                </span>
+              </span>
+              <span>
+                <p>A venir</p>
+              </span>
+            </div>
+            <div class="cardv2rank"></div>
+          </div>`
+        });
+
+        putGames.innerHTML = game;
+
+
+        const PercentMatching = () => {
+
+            const elements = document.querySelectorAll('.cardV2RatingPourcentage');
+
+            elements.forEach(function (element) {
+
+                let size = extractNumber(getValueFromElement(element));
+
+                const bar = element.closest(".cardV2RatingBar");
+
+                // Vérifie que la valeur récupérée est un nombre et que ce nombre est en dessous de la valeur indiquée
+                if (!isNaN(size) && size >= 66) {
+                    // Convertit la valeur en %
+                    const width = `${size}%`;
+                    bar.style.width = width;
+                    $(bar).addClass("green");
+
+                } else if (size <= 66 && size >= 33) {
+                    const width = `${size}%`;
+                    bar.style.width = width;
+                    $(bar).addClass("orange");
+
+                } else if (size <= 33 && size >= 15) {
+                    const width = `${size}%`;
+                    bar.style.width = width;
+                    $(bar).addClass("red");
+
+                } else {
+                    size = 20;
+
+                    const width = `${size}%`;
+                    bar.style.width = width;
+                    $(bar).addClass("red");
+
+                }
+            });
+        }
+
+        function getValueFromElement(elementSelector) {
+            const element = $(elementSelector);
+
+            // Vérifie que l'élément existe et contient du texte
+            if (element.length && element.text().trim().length > 0) {
+                const value = element.text().trim();
+
+                return value;
+            }
+
+            // Retourne null si l'élément n'existe pas ou ne contient pas de texte
+            return null;
+        }
+
+        function extractNumber(str) {
+            if (str && str.includes('%')) {
+                const number = parseFloat(str.replace('%', ''));
+
+                if (!isNaN(number)) {
+                    return number;
+                }
+            }
+            return null;
+        }
+        PercentMatching();
+
+        // Développer la fiche d'un jeu sur la page d'accueil
+        const DescDevelop = () => {
+            $(document).ready(function () {
+                // Détection du click
+                $('.cardBtn').click(function (e) {
+                    // Ajout de la classe au premier click, remove au second
+                    $(e.target).closest('.cardBtn').toggleClass('btn-clicked');
+                    $(e.target).closest('.cardv2').toggleClass('card-active');
+                });
+            });
+        }
+        DescDevelop();
+    };
+
+    printGames();
+}
+
+showListProduct();
