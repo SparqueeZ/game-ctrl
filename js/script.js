a32c3f93c5d76f9c959d26f2bde7286bcef15356
Baptiste <baptiste.dijouxn8@gmail.com>
2023-04-19 00:29:51 +0200

    Ajout : Verification afin de savoir si un element du jeu est vide, si c'est le cas, il ne sera pas diffusé sur la page, sinon oui. 2. Icons pour les plateformes 3. Indisponible pour le moment sur les détails des cartes de jeux Modifications : 1. Reorganisation du code JS

--- js/script.js
+++ js/script.js
@@ -14,8 +14,27 @@ const Slider = () => {
   }
 }
 
+var date = new Date()
+console.log("Nous sommes le : ");
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let lol = date.getTime();
+console.log(lol);
+
+var date2 = new Date('07/02/2023');
+let lol2 = date2.getTime();
+console.log(lol2);
+
+if (month < 10) {
+  console.log(day + "/" + 0 + month + "/" + year);
+} else {
+  console.log(day + "/" + month + "/" + year);
+}
+
 Slider();
-// On cherche ici le nom de notre page
+// On cherche ici l'adresse de notre page
 var loc = $(location).attr('href');
 generateHeader(loc);
 generateFooter(loc);
--- js/showGames.js
+++ js/showGames.js
@@ -1,20 +1,104 @@
 const showListProduct = () => {
 
-    const dataJeux = get('http://localhost:3000/api/v1/jeux')
-        .then((response) => response.json())
-        .then((data) => {
-            return data;
-        });
+  const Container = document.getElementById('container');
+  let placeholder = '';
+  for (let i = 0; i < 5; i++) {
+    placeholder += `<div class="cardv2">
+      <div class="card-left">
+        <span class="cardPlaceholderImg">
+          <img src="img/icons/noun-picture-4875198.svg" alt="">
+        </span>
+      </div>
+      <div class="card-right">
+        <span class="cardv2name">
+          <h3><span class="textReplacer"></span></h3>
+        </span>
+        <p class="cardv2phrase"><span class="textReplacer"></span><span class="textReplacer"></span></p>
+        <span class="cardV2RatingContainer">
+          <span class="cardV2RatingSection">
+            <span class="cardV2RatingIcon">
+              <span class="cardPlaceholderIcon">
+                <img src="img/icons/noun-picture-4875198.svg" alt=""></span>
+            </span>
+            <span class="cardV2RatingProgressBar">
+              <span class="cardRatingIconBg"></span>
+              <span class="cardRatingBarHolder">
+              </span>
+            </span>
+          </span>
+          <span class="cardV2RatingSection">
+            <span class="cardV2RatingIcon">
+              <span class="cardPlaceholderIcon">
+                <img src="img/icons/noun-picture-4875198.svg" alt=""></span>
+            </span>
+            <span class="cardV2RatingProgressBar">
+              <span class="cardRatingIconBg"></span>
+              <span class="cardRatingBarHolder">
+              </span>
+            </span>
+          </span>
+          <span class="cardV2RatingSection">
+            <span class="cardV2RatingIcon">
+              <span class="cardPlaceholderIcon">
+                <img src="img/icons/noun-picture-4875198.svg" alt=""></span>
+            </span>
+            <span class="cardV2RatingProgressBar">
+              <span class="cardRatingIconBg"></span>
+              <span class="cardRatingBarHolder">
+              </span>
+            </span>
+          </span>
+          <span class="cardBtnSection">
+            <span class="cardBtnHolder">
+              <span class="cardBtn">
+              </span>
+            </span>
+          </span>
+        </span>
+      </div>
+    </div>`
+  }
+  Container.innerHTML = placeholder;
 
-    // Utilisation de l'asynchrone avec async await afin d'attendre la réponse de la promise avant d'éxécuter le code
-    const printGames = async () => {
-        const liste = await dataJeux;
-        console.log(liste.data[1].nom); //Accéder au nom du 2e element du tableau
+  const dataJeux = get('http://localhost:3000/api/v1/jeux')
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
 
-        const putGames = document.getElementById('container');
-        let game = '';
-        liste.data.forEach(jeu => {
-            game += `
+  // Utilisation de l'asynchrone avec async await afin d'attendre la réponse de la promise avant d'éxécuter le code
+  const printGames = async () => {
+    const liste = await dataJeux;
+    console.log(liste.data[1].nom); //Accéder au nom du 2e element du tableau
+
+    const Container = document.getElementById('container');
+
+    let game = '';
+    liste.data.forEach(jeu => {
+      console.log(jeu.plateforme);
+
+      const insert = (element) => {
+        if (element == undefined || element == '' || element == 'N/A') {
+          return '';
+        } else if (element == "playstation") {
+          element = "img/icons/plateforme_playstation.svg";
+          return `
+          <li class="gameLi"><img src="${element}" class="cardBubble gamePlatform" alt=""></li>`
+        } else if (element == 'pc') {
+          element = "img/icons/plateforme_pc.svg";
+          return `
+          <li class="gameLi"><img src="${element}" class="cardBubble gamePlatform" alt=""></li>`
+        } else if (element == 'xbox') {
+          element = "img/icons/plateforme_xbox.svg";
+          return `
+          <li class="gameLi"><img src="${element}" class="cardBubble gamePlatform" alt=""></li>`
+        } else {
+          return `<li class="gameLi"><p class="cardBubble gameDate">${element}</p></li>`
+            ;
+        }
+      }
+
+      game += `
           <div class="cardv2">
             <div class="card-left">
               <img class="cardv2img" src="${jeu.img_miniature}" alt="" />
@@ -26,7 +110,14 @@ const showListProduct = () => {
               </span>
             </div>
             <div class="card-right">
-              <span class="cardv2name"><h3>${jeu.nom}</h3><p>${jeu.nom_precision}</p></span>
+              <span class="cardv2name"><h3>${jeu.nom}</h3><span class="cardGameDetails">
+              ${insert(jeu.date_de_sortie)}
+              ${insert(jeu.plateforme)}
+              ${insert(jeu.version)}
+              ${insert(jeu.developper)}
+              
+              </span>
+              </span>
               <p class="cardv2phrase">${jeu.description_rapide}</p>
               <span class="cardV2RatingContainer">
                 <span class="cardV2RatingSection">
@@ -74,96 +165,104 @@ const showListProduct = () => {
                   </span>
                 </span>
               </span>
-              <span>
-                <p>A venir</p>
+              <span class="warningMessage">
+                <img src="img/icons/Warning.svg">
+                <p>INDISPONIBLE</p>
               </span>
             </div>
             <div class="cardv2rank"></div>
           </div>`
+
+      console.log(jeu.version);
+
+      const test = () => {
+        document.querySelectorAll(".cardGameDetails").forEach(function (e) {
+
+          console.log(e);
+          // if (jeu.version == "") {
+          //   console.log("Supprimer");
+          // }
+        })
+      }
+      test();
+    });
+
+
+    Container.innerHTML = game;
+
+    const PercentMatching = () => {
+      const elements = document.querySelectorAll('.cardV2RatingPourcentage');
+
+      elements.forEach(function (element) {
+        let size = extractNumber(getValueFromElement(element));
+        const bar = element.closest(".cardV2RatingBar");
+
+        // Vérifie que la valeur récupérée est un nombre et que ce nombre est en dessous de la valeur indiquée
+        if (!isNaN(size) && size >= 66) {
+          // Convertit la valeur en %
+          const width = `${size}%`;
+          bar.style.width = width;
+          $(bar).addClass("green");
+
+        } else if (size <= 66 && size >= 33) {
+          const width = `${size}%`;
+          bar.style.width = width;
+          $(bar).addClass("orange");
+
+        } else if (size <= 33 && size >= 15) {
+          const width = `${size}%`;
+          bar.style.width = width;
+          $(bar).addClass("red");
+
+        } else {
+          size = 20;
+          const width = `${size}%`;
+          bar.style.width = width;
+          $(bar).addClass("red");
+        }
+      });
+    }
+
+    function getValueFromElement(elementSelector) {
+      const element = $(elementSelector);
+
+      // Vérifie que l'élément existe et contient du texte
+      if (element.length && element.text().trim().length > 0) {
+        const value = element.text().trim();
+        return value;
+      }
+
+      // Retourne null si l'élément n'existe pas ou ne contient pas de texte
+      return null;
+    }
+
+    function extractNumber(str) {
+      if (str && str.includes('%')) {
+        const number = parseFloat(str.replace('%', ''));
+
+        if (!isNaN(number)) {
+          return number;
+        }
+      }
+      return null;
+    }
+    PercentMatching();
+
+    // Développer la fiche d'un jeu sur la page d'accueil
+    const DescDevelop = () => {
+      $(document).ready(function () {
+        // Détection du click
+        $('.cardBtn').click(function (e) {
+          // Ajout de la classe au premier click, remove au second
+          $(e.target).closest('.cardBtn').toggleClass('btn-clicked');
+          $(e.target).closest('.cardv2').toggleClass('card-active');
         });
+      });
+    }
+    DescDevelop();
+  };
 
-        putGames.innerHTML = game;
-
-
-        const PercentMatching = () => {
-
-            const elements = document.querySelectorAll('.cardV2RatingPourcentage');
-
-            elements.forEach(function (element) {
-
-                let size = extractNumber(getValueFromElement(element));
-
-                const bar = element.closest(".cardV2RatingBar");
-
-                // Vérifie que la valeur récupérée est un nombre et que ce nombre est en dessous de la valeur indiquée
-                if (!isNaN(size) && size >= 66) {
-                    // Convertit la valeur en %
-                    const width = `${size}%`;
-                    bar.style.width = width;
-                    $(bar).addClass("green");
-
-                } else if (size <= 66 && size >= 33) {
-                    const width = `${size}%`;
-                    bar.style.width = width;
-                    $(bar).addClass("orange");
-
-                } else if (size <= 33 && size >= 15) {
-                    const width = `${size}%`;
-                    bar.style.width = width;
-                    $(bar).addClass("red");
-
-                } else {
-                    size = 20;
-
-                    const width = `${size}%`;
-                    bar.style.width = width;
-                    $(bar).addClass("red");
-
-                }
-            });
-        }
-
-        function getValueFromElement(elementSelector) {
-            const element = $(elementSelector);
-
-            // Vérifie que l'élément existe et contient du texte
-            if (element.length && element.text().trim().length > 0) {
-                const value = element.text().trim();
-
-                return value;
-            }
-
-            // Retourne null si l'élément n'existe pas ou ne contient pas de texte
-            return null;
-        }
-
-        function extractNumber(str) {
-            if (str && str.includes('%')) {
-                const number = parseFloat(str.replace('%', ''));
-
-                if (!isNaN(number)) {
-                    return number;
-                }
-            }
-            return null;
-        }
-        PercentMatching();
-
-        // Développer la fiche d'un jeu sur la page d'accueil
-        const DescDevelop = () => {
-            $(document).ready(function () {
-                // Détection du click
-                $('.cardBtn').click(function (e) {
-                    // Ajout de la classe au premier click, remove au second
-                    $(e.target).closest('.cardBtn').toggleClass('btn-clicked');
-                    $(e.target).closest('.cardv2').toggleClass('card-active');
-                });
-            });
-        }
-        DescDevelop();
-    };
-
-    printGames();
+  setTimeout(printGames, 1000);
 }
 
 showListProduct();
